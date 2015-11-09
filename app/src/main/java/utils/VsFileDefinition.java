b5e396391612285bce82565a1dfd14105da9cc90
hughie <530125717@qq.com>
2015-11-09 15:02:38 +0800

    1. Rename VsFileOP.java to VsFileUtility.java. 2. Add judgeProgramFile function: Create file(text file) with given directory path and file-name, e.g. (/sdcard0/Giant/program,P9999).         * @param dirPath Given directory path of program-file.         * @param proFileName File-name need to be judged under dirPath.

---
+++ app/src/main/java/utils/VsFileDefinition.java
@@ -0,0 +1,11 @@
+package utils;
+
+import android.os.Environment;
+
+/**
+ * Created by hughie on 15/11/9.
+ */
+public class VsFileDefinition
+{
+    public static final char CHAR_SLASH = '/';
+}
--- app/src/main/java/utils/VsFileUtility.java
+++ app/src/main/java/utils/VsFileUtility.java
@@ -1,22 +1,23 @@
 package utils;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by hughie on 15/10/23.
  */
-public class VsFileOP
+public class VsFileUtility
 {
     /**
      * Some useful GFileUtility defines.
      * */
-    public static int FILE_UNDEFINED = 0;
-    public static int FILE_EXIST = 1;
-    public static int FILE_NOTEXIST = -1;
-    public static int FILE_CREATE_SUCCEED = 2;
-    public static int FILE_CREATE_FAILED = -2;
-    public static int FILE_DELETE_SUCCEED = 3;
-    public static int FILE_DELETE_FAILED = -3;
+    public static final int FILE_UNDEFINED = 0;
+    public static final int FILE_EXIST = 1;
+    public static final int FILE_NOTEXIST = -1;
+    public static final int FILE_CREATE_SUCCEED = 2;
+    public static final int FILE_CREATE_FAILED = -2;
+    public static final int FILE_DELETE_SUCCEED = 3;
+    public static final int FILE_DELETE_FAILED = -3;
 
     /**
      * Delete sub directories and files of file.
@@ -103,5 +104,46 @@ public class VsFileOP
             return FILE_CREATE_FAILED;
 
         return FILE_CREATE_SUCCEED;
+    }
+    /**
+     * See: public static int judgeDirectory(final String dirPath)
+     * */
+    public static boolean judgeDirectoryCheck(final String dirPath)
+    {
+        int jude = judgeDirectory(dirPath);
+        if(jude == FILE_EXIST || jude == FILE_CREATE_SUCCEED)
+            return true;
+        return false;
+    }
+    /**
+     * Create file(text file) with given directory path and file-name, e.g. (/sdcard0/Giant/program,P9999).
+     * @param dirPath Given directory path of program-file.
+     * @param proFileName File-name need to be judged under dirPath.
+     * */
+    public static int judgeProgramFile(final String dirPath, final String proFileName)
+    {
+        int ret = judgeDirectory(dirPath);
+        if(ret != FILE_EXIST && ret != FILE_CREATE_SUCCEED)
+            return ret;
+        File file = new File(dirPath + VsFileDefinition.CHAR_SLASH + proFileName);
+        try
+        {
+            if(file.createNewFile())
+                return FILE_CREATE_SUCCEED;
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return FILE_UNDEFINED;
+    }
+    /**
+     * See: public static int judgeProgramFile(final String dirPath, final String proFileName)
+     * */
+    public static boolean judgeProgramFileCheck(final String dirPath, final String proFileName)
+    {
+        int jude = judgeProgramFile(dirPath,proFileName);
+        if(jude == FILE_EXIST || jude == FILE_CREATE_SUCCEED)
+            return true;
+        return false;
     }
 }
