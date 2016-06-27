e977bcf87a2dd10e4c77018fb2d426c015fcbf10
hughie <530125717@qq.com>
2016-06-27 10:43:53 +0800

    Add comment of File.separator

--- app/src/main/java/utils/VsFileDefinition.java
+++ app/src/main/java/utils/VsFileDefinition.java
@@ -2,10 +2,12 @@ package utils;
 
 import android.os.Environment;
 
+import java.io.File;
+
 /**
  * Created by hughie on 15/11/9.
  */
 public class VsFileDefinition
 {
-    public static final char CHAR_SLASH = '/';
+    public static final char CHAR_SLASH = '/'; ///< File.separator(String)  File.separatorChar(char)
 }
